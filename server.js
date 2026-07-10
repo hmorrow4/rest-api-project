a77a42196b1b8249d78f24df674a6251578f36cb
Hunter Morrow <huntermorrow@Hunters-MacBook-Air-3.local>
2026-07-09 19:22:51 -0500

    Implement GET resource by ID endpoint

--- server.js
+++ server.js
@@ -24,6 +24,20 @@ app.get("/api/resources", (req, res) => {
   res.json(resources);
 });
 
+app.get("/api/resources/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const resource = resources.find((item) => item.id === id);
+
+  if (!resource) {
+    return res.status(404).json({
+      message: "Resource not found"
+    });
+  }
+
+  res.json(resource);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
